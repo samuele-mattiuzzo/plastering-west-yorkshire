34d76ce987d5aec70eb9c3dbcd3fc41104ae12a9
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-03-31 17:32:28 +0100

    initial2

--- plastering-app/src/app/app.component.ts
+++ plastering-app/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    ServicesComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'plastering-app';
 }
--- plastering-app/src/app/footer/footer.component.ts
+++ plastering-app/src/app/footer/footer.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
 }
